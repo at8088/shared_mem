ca79b02cb6d283c5fa86aecbe976d37a0f198e21
Francois Broquedis <francois.broquedis@imag.fr>
2016-11-08 10:08:45 +0100

    simplification put_pixel + commentaires dans le code.

--- src/shmem_pong.c
+++ src/shmem_pong.c
@@ -16,7 +16,7 @@
 const int TAILLE_X = 800;
 const int TAILLE_Y = 600;
 
-/* parametres de l'affichage du carre */
+/* parametres de l'affichage de la balle */
 static int x;
 static int y;
 static int delta_x;
@@ -32,35 +32,14 @@ static int couleur;
 
 static void put_pixel(SDL_Surface *surface, int x, int y, uint32_t pixel)
 {
+    /*
+      Remarque : le nombre d'octets par pixel est toujours 4 dans ce programme,
+      ce qui simplifie grandement l'écriture de put_pixel().
+     */
     int bpp = surface->format->BytesPerPixel;
-    /* Here p is the address to the pixel we want to set */
+    /* p est l'adresse du pixel qu'on veut modifier. */
     uint8_t *p = (uint8_t *) surface->pixels + y * surface->pitch + x * bpp;
-
-    switch (bpp) {
-    case 1:
-        *p = pixel;
-        break;
-
-    case 2:
-        *(uint16_t *) p = pixel;
-        break;
-
-    case 3:
-        if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-            p[0] = (pixel >> 16) & 0xff;
-            p[1] = (pixel >> 8) & 0xff;
-            p[2] = pixel & 0xff;
-        } else {
-            p[0] = pixel & 0xff;
-            p[1] = (pixel >> 8) & 0xff;
-            p[2] = (pixel >> 16) & 0xff;
-        }
-        break;
-
-    case 4:
-        *(uint32_t *) p = pixel;
-        break;
-    }
+    *(uint32_t *) p = pixel;
 }
 
 static void draw_ball(SDL_Surface *canvas)
@@ -113,14 +92,25 @@ int main(int argc, char **argv)
     if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
         handle_SDL_error("Init");
 
+    /* On crée une nouvelle fenêtre (un écran) SDL. */
     ecran = SDL_SetVideoMode(TAILLE_X, TAILLE_Y, 32, SDL_SWSURFACE);
     if (ecran == NULL)
         handle_SDL_error("SetVideoMode");
 
+    /* On donne un nom à la fenêtre SDL. */
     SDL_WM_SetCaption("Mini carrés", NULL);
 
+    /*
+       On initialise le générateur de nombres aléatoires en utilisant
+       le pid du processus courant comme graine. Ainsi, chaque nouvelle
+       exécution du programme donnera des nombres différents.
+    */
     srandom(getpid());
 
+    /*
+       On initialise les caractéristiques de la balle : position initiale,
+       taille, couleur. Ces valeurs sont tirées aléatoirement.
+    */
     x = random() % TAILLE_X;
     y = random() % TAILLE_Y;
     delta_x = random() % 5 + 1;
@@ -133,10 +123,11 @@ int main(int argc, char **argv)
    * Le tampon a changer pour mettre en place le couplage mémoire
    * entre les différents processus.
    *********************/
-    void *tampon = malloc(TAILLE_X * TAILLE_Y * 4);
+    void *tampon = calloc(TAILLE_X * TAILLE_Y, 4);
     if (tampon == NULL)
         handle_error("malloc");
 
+    /* On associe ce tampon à la zone de dessin. */
     canvas =
         SDL_CreateRGBSurfaceFrom(tampon, TAILLE_X, TAILLE_Y, 32, TAILLE_X * 4,
                                  0x0, 0x0, 0x0, 0x0);
@@ -145,23 +136,35 @@ int main(int argc, char **argv)
 
     assert(canvas->pixels == tampon);
 
+    /*
+       On demande à SDL d'afficher cette zone de dessin
+       sur l'écran initialisé plus haut.
+    */
     if (SDL_BlitSurface(canvas, NULL, ecran, NULL) == -1)
         handle_SDL_error("BlitSurface");
 
     if (SDL_Flip(ecran) == -1)
         handle_SDL_error("Flip");
 
+    /* Boucle infinie d'affichage de l'animation de la balle. */
     while (1) {
+        /* On attend 20ms entre chaque affichage. */
         SDL_Delay(20);
 
+        /* On dessine la nouvelle position de la balle. */
         draw_ball(canvas);
 
+        /* On affiche la zone de dessin correspondante sur l'écran. */
         if (SDL_BlitSurface(canvas, NULL, ecran, NULL) == -1)
             handle_SDL_error("BlitSurface");
 
         if (SDL_Flip(ecran) == -1)
             handle_SDL_error("Flip");
 
+        /*
+           Si l'utilisateur clique sur la croix pour fermer la fenêtre,
+           on s'arrête proprement.
+         */
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
                 goto fin;
@@ -169,6 +172,7 @@ int main(int argc, char **argv)
     }
 
   fin:
+    /* On libère la mémoire avant de s'arrêter. */
     SDL_FreeSurface(canvas);
     free(tampon);
     SDL_Quit();
