36973216852b23c42f51036d5423a6b2d8575f11
Francois Broquedis <francois.broquedis@imag.fr>
2016-11-08 09:44:28 +0100

    coding style projet C.

--- src/shmem_pong.c
+++ src/shmem_pong.c
@@ -8,12 +8,21 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <SDL/SDL.h>
 #include <assert.h>
 
-const int TAILLEX = 800;
-const int TAILLEY = 600;
+const int TAILLE_X = 800;
+const int TAILLE_Y = 600;
+
+/* parametres de l'affichage du carre */
+static int x;
+static int y;
+static int delta_x;
+static int delta_y;
+static int taille;
+static int couleur;
 
 #define handle_error(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
@@ -21,12 +30,11 @@ const int TAILLEY = 600;
 #define handle_SDL_error(msg) \
   do { fprintf(stderr, "SDL :" msg ", ligne %d \n", __LINE__); exit(EXIT_FAILURE); } while (0)
 
-
-void putpixel(SDL_Surface * surface, int x, int y, Uint32 pixel)
+static void put_pixel(SDL_Surface *surface, int x, int y, uint32_t pixel)
 {
     int bpp = surface->format->BytesPerPixel;
     /* Here p is the address to the pixel we want to set */
-    Uint8 *p = (Uint8 *) surface->pixels + y * surface->pitch + x * bpp;
+    uint8_t *p = (uint8_t *) surface->pixels + y * surface->pitch + x * bpp;
 
     switch (bpp) {
     case 1:
@@ -34,7 +42,7 @@ void putpixel(SDL_Surface * surface, int x, int y, Uint32 pixel)
         break;
 
     case 2:
-        *(Uint16 *) p = pixel;
+        *(uint16_t *) p = pixel;
         break;
 
     case 3:
@@ -50,57 +58,49 @@ void putpixel(SDL_Surface * surface, int x, int y, Uint32 pixel)
         break;
 
     case 4:
-        *(Uint32 *) p = pixel;
+        *(uint32_t *) p = pixel;
         break;
     }
 }
 
-/* parametre de l'affichage du carre */
-int x;
-int y;
-int deltax;
-int deltay;
-int taille;
-int couleur;
-
-void draw_ball(SDL_Surface * canvas)
+static void draw_ball(SDL_Surface *canvas)
 {
     /* fond noir sur l'ancienne position */
     int noir = 0;
     for (int k = 0; k < taille; k++)
         for (int l = 0; l < taille; l++)
-            putpixel(canvas, x + k, y + l, noir);
+            put_pixel(canvas, x + k, y + l, noir);
 
 
     /* modification de position */
-    x += deltax;
-    y += deltay;
+    x += delta_x;
+    y += delta_y;
 
     /* inversion des directions et ajustement */
-    if (x + taille > TAILLEX) {
-        x = TAILLEX - taille;
-        deltax *= -1;
+    if (x + taille > TAILLE_X) {
+        x = TAILLE_X - taille;
+        delta_x *= -1;
     }
 
     if (x < 0) {
         x = 0;
-        deltax *= -1;
+        delta_x *= -1;
     }
 
-    if (y + taille > TAILLEY) {
-        y = TAILLEY - taille;
-        deltay *= -1;
+    if (y + taille > TAILLE_Y) {
+        y = TAILLE_Y - taille;
+        delta_y *= -1;
     }
 
     if (y < 0) {
         y = 0;
-        deltay *= -1;
+        delta_y *= -1;
     }
 
     /* dessin de la balle */
     for (int k = 0; k < taille; k++)
         for (int l = 0; l < taille; l++)
-            putpixel(canvas, x + k, y + l, couleur);
+            put_pixel(canvas, x + k, y + l, couleur);
 }
 
 
@@ -113,7 +113,7 @@ int main(int argc, char **argv)
     if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
         handle_SDL_error("Init");
 
-    ecran = SDL_SetVideoMode(TAILLEX, TAILLEY, 32, SDL_SWSURFACE);
+    ecran = SDL_SetVideoMode(TAILLE_X, TAILLE_Y, 32, SDL_SWSURFACE);
     if (ecran == NULL)
         handle_SDL_error("SetVideoMode");
 
@@ -121,10 +121,10 @@ int main(int argc, char **argv)
 
     srandom(getpid());
 
-    x = random() % TAILLEX;
-    y = random() % TAILLEY;
-    deltax = random() % 5 + 1;
-    deltay = random() % 5 + 1;
+    x = random() % TAILLE_X;
+    y = random() % TAILLE_Y;
+    delta_x = random() % 5 + 1;
+    delta_y = random() % 5 + 1;
     taille = random() % 20 + 10;
     couleur =
         (random() % 256 << 16) | (random() % 256 << 8) | (random() % 255);
@@ -133,12 +133,12 @@ int main(int argc, char **argv)
    * Le tampon a changer pour mettre en place le couplage mémoire
    * entre les différents processus.
    *********************/
-    void *tampon = malloc(TAILLEX * TAILLEY * 4);
+    void *tampon = malloc(TAILLE_X * TAILLE_Y * 4);
     if (tampon == NULL)
         handle_error("malloc");
 
     canvas =
-        SDL_CreateRGBSurfaceFrom(tampon, TAILLEX, TAILLEY, 32, TAILLEX * 4,
+        SDL_CreateRGBSurfaceFrom(tampon, TAILLE_X, TAILLE_Y, 32, TAILLE_X * 4,
                                  0x0, 0x0, 0x0, 0x0);
     if (canvas == NULL)
         handle_SDL_error();
